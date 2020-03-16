0ffa7d90289aca4270ca9cf3d41e9e3f683f67b5
janecheng <jane.zheng@leishan.com.tw>
2020-03-16 16:56:39 +0800

    initial

---
+++ getEmailLabels.php
@@ -0,0 +1,18 @@
+<?php
+use Symfony\Component\Yaml\Yaml;
+
+require __DIR__ . '/quickstart.php';
+
+$client = getClient();
+$service = new Google_Service_Gmail($client);
+$config = Yaml::parse(file_get_contents(__DIR__ . "/config/getEmailAttachment.yaml"));
+
+$user = $config['user'];
+$labels = $service->users_labels->listUsersLabels($user);
+$labels_arr = [];
+
+foreach ($labels as $key => $value) {
+    $labels_arr[$key]['id'][] = $value->id;
+    $labels_arr[$key]['name'][] = $value->name;
+}
+return $labels_arr;
