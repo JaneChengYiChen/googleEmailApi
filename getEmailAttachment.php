1ebeb1501a4dd38cd00c7e010bab48db7089d675
janecheng <jane.zheng@leishan.com.tw>
2020-03-17 17:51:19 +0800

    revise list quest

--- getEmailAttachment.php
+++ getEmailAttachment.php
@@ -8,33 +8,34 @@ $service = new Google_Service_Gmail($client);
 $config = Yaml::parse(file_get_contents(__DIR__ . "/config/getEmailAttachment.yaml"));
 
 $user = $config['user'];
-$messages = $service->users_messages->listUsersMessages($user);
+$messages = $service->users_messages->listUsersMessages(
+    $user,
+    array(
+        "labelIds" => $config['label'],
+        "maxResults" => 50)
+);
 $emailAccount = $config['email_account'];
 
 define('UPLOAD_DIR', $config['upload_dir']);
 
 foreach ($messages as $messages) {
-    # Label_1085736184640390570 --money laundering
     $message = $service->users_messages->get($emailAccount, $messages->getId());
-    $label = $message->labelIds;
-    $specific_label = array_keys($label, $config['label']);
 
-    if (!empty($specific_label)) {
-        $attachmentId = $message->payload->parts[1]->body->attachmentId;
-        if (!is_null($attachmentId)) {
-            //get attachments
-            $fileName = $message->payload->parts[1]->filename;
-            $attachmentObj = $service->users_messages_attachments->get(
-                $emailAccount,
-                $messages->getId(),
-                $attachmentId
-            );
-            $data = $attachmentObj->getData();
-            $data = strtr($data, array('-' => '+', '_' => '/'));
-            $file = UPLOAD_DIR . $fileName;
-            file_put_contents($file, base64_decode($data));
+    $attachmentId = $message->payload->parts[1]->body->attachmentId;
+    if (!is_null($attachmentId)) {
+        //get attachments
+        $fileName = $message->payload->parts[1]->filename;
+        $attachmentObj = $service->users_messages_attachments->get(
+            $emailAccount,
+            $messages->getId(),
+            $attachmentId
+        );
+        $data = $attachmentObj->getData();
+        $data = strtr($data, array('-' => '+', '_' => '/'));
+        $file = UPLOAD_DIR . $fileName;
+        file_put_contents($file, base64_decode($data));
 
-            echo 'successful';
-        }
+        echo 'successful';
     }
+
 }
